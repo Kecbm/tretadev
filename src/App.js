080a5a68572da6029bd0f5384e8af4aeac1ec248
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-15 16:36:56 -0300

    :sparkles: implementando tag de filtragens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [issues, setIssues] = useState([]);
+  const [filteredIssues, setFilteredIssues] = useState([]);
+  const [selectedTag, setSelectedTag] = useState('todas');
 
   useEffect(() => {
     async function fetchIssues() {
@@ -26,6 +28,7 @@ function App() {
         })
 
         setIssues(issuesFormated);
+        setFilteredIssues(issuesFormated);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -41,11 +44,115 @@ function App() {
     return brasilFormattedDate;
   }
 
+  const handleTagClick = (tags) => {
+    setSelectedTag(tags);
+  
+    if (tags.includes('todas')) {
+      setFilteredIssues(issues);
+    } else {
+      const filtered = issues.filter((issue) =>
+        tags.some((tag) =>
+          (issue.title && issue.title.toLowerCase().includes(tag)) ||
+          (issue.body && issue.body.toLowerCase().includes(tag))
+        )
+      );
+  
+      setFilteredIssues(filtered);
+    }
+  };  
+  
   return (
     <div className="App">
       <h1>Treta Dev 🔥</h1>
+      <div>
+        <button>
+          <a href="https://github.com/lincolixavier/tretadev/issues/new" target="_blank" rel="noopener noreferrer">
+            Adicionar treta
+          </a>
+        </button>
+      </div>
+      <div>
+        <button onClick={() => handleTagClick(['todas'])}>
+          Todas
+        </button>
+        <button onClick={() => handleTagClick(['backend', 'back', 'api', 'heroku', 'mongodb', 'nosql', 'sql'])}>
+          Backend
+        </button>
+        <button onClick={() => handleTagClick(['frontend', 'front', 'botão', 'react', 'reactjs', 'angular', 'vue', 'vuejs','tailwind'])}>
+          Frontend
+        </button>
+        <button onClick={() => handleTagClick(['dados', 'mongodb', 'nosql', 'sql'])}>
+          Dados
+        </button>
+        <button onClick={() => handleTagClick(['teste', 'tester'])}>
+          QA
+        </button>
+        <button onClick={() => handleTagClick(['open-source', 'open source'])}>
+          Open-source
+        </button>
+        <button onClick={() => handleTagClick(['junior'])}>
+          Junior
+        </button>
+        <button onClick={() => handleTagClick(['pleno'])}>
+          Pleno
+        </button>
+        <button onClick={() => handleTagClick(['senior'])}>
+          Senior
+        </button>
+        <button onClick={() => handleTagClick(['sistema operacional', 'linux', 'windows', 'ubuntu'])}>
+          Sistema operacional
+        </button>
+        <button onClick={() => handleTagClick(['vim', 'nvim'])}>
+          IDE
+        </button>
+        <button onClick={() => handleTagClick(['html'])}>
+          HTML
+        </button>
+        <button onClick={() => handleTagClick(['css'])}>
+          CSS
+        </button>
+        <button onClick={() => handleTagClick(['javascript', 'react', 'reactjs'])}>
+          JavaScript
+        </button>
+        <button onClick={() => handleTagClick(['php'])}>
+          PHP
+        </button>
+        <button onClick={() => handleTagClick(['python'])}>
+          Python
+        </button>
+        <button onClick={() => handleTagClick(['java'])}>
+          Java
+        </button>
+        <button onClick={() => handleTagClick(['django'])}>
+          Django
+        </button>
+        <button onClick={() => handleTagClick(['flutter'])}>
+          Flutter
+        </button>
+        <button onClick={() => handleTagClick(['design', 'ui', 'ux'])}>
+          Design
+        </button>
+        <button onClick={() => handleTagClick(['commit'])}>
+          Commit
+        </button>
+        <button onClick={() => handleTagClick(['processo seletivo', 'burnout', 'emprego', 'vaga', 'dev', 'devs', 'profissão', 'fit cultural', 'recrutador', 'linkedin', 'github', 'salário', 'daily', 'empresa', 'engenheiro', 'mil', 'carreira', 'salário', 'clt', 'pj'])}>
+          Carreira
+        </button>
+        <button onClick={() => handleTagClick(['tecnologia', 'programação', 'programar', 'programa', 'programador', 'low-code', 'low code', 'cookies', 'computação'])}>
+          Tecnologia
+        </button>
+        <button onClick={() => handleTagClick(['faculdade'])}>
+          Faculdade
+        </button>
+        <button onClick={() => handleTagClick(['gambiarra'])}>
+          Gambiarra
+        </button>
+        <button onClick={() => handleTagClick(['esquerda', 'direita'])}>
+          Politica
+        </button>
+      </div>
       <ul>
-        {issues.map(issue => (
+        {filteredIssues.map(issue => (
           <li key={issue.id}>
             <h2>{issue.title}</h2>
             <p>{issue.body}</p>
